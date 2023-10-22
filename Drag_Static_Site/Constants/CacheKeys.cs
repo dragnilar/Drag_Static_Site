06fcb9202d4e8c9688b51a6ac6d3d2e8ce2e7ddc
Stevie White <stephenjeffreywhite@outlook.com>
2023-10-22 00:02:04 -0400

    Added a quasi-pipeline

---
+++ Drag_Static_Site/Constants/CacheKeys.cs
@@ -0,0 +1,7 @@
+﻿namespace Drag_Static_Site.Constants
+{
+    public static class CacheKeys
+    {
+        public static string MainLayout_ShowDrawer = "MainLayout_ShowDrawer";
+    }
+}
--- Drag_Static_Site/Program.cs
+++ Drag_Static_Site/Program.cs
@@ -1,11 +1,20 @@
+using Blazored.LocalStorage;
 using Drag_Static_Site;
+using Drag_Static_Site.Constants;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddMudServices();
+builder.Services.AddMudMarkdownServices();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddSingleton<PostCollection>();
 
 await builder.Build().RunAsync();
+
