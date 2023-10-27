0fe647614fa30d865c22cda60f5293508d122e74
Stevie White <stephenjeffreywhite@outlook.com>
2023-10-26 23:54:22 -0400

    Remove Pre-Build Utils, replaced with pre-build event for that too

---
+++ Drag_Static_Site/Models/GridItem.cs
@@ -0,0 +1,25 @@
+﻿namespace Drag_Static_Site.Models;
+
+public class GridItem
+{
+    public int     number1 { get; set; }
+    public int     number2 { get; set; }
+    public int     number3 { get; set; }
+    public int     number4 { get; set; }
+    public int     number5 { get; set; }
+    public string  string1 { get; set; }
+    public string  string2 { get; set; }
+    public string  string3 { get; set; }
+    public string  string4 { get; set; }
+    public string  string5 { get; set; }
+    public decimal dec1    { get; set; }
+    public decimal dec2    { get; set; }
+    public decimal dec3    { get; set; }
+    public decimal dec4    { get; set; }
+    public decimal dec5    { get; set; }
+    public bool    bool1   { get; set; }
+    public bool    bool2   { get; set; }
+    public bool    bool3   { get; set; }
+    public bool    bool4   { get; set; }
+    public bool    bool5   { get; set; }
+}
