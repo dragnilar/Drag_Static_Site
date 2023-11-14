fb515daec5a8bc5f7ae0fa18eb7a21579886dffc
Stevie White <stephenjeffreywhite@outlook.com>
2023-11-14 01:41:42 -0500

    Added combat and 11/14/2023 post

---
+++ Drag_Static_Site/Models/CombatMonster.cs
@@ -0,0 +1,10 @@
+﻿namespace Drag_Static_Site.Models;
+
+public class CombatMonster
+{
+    public string MonsterName    { get; set; }
+    public int    MonsterHp      { get; set; }
+    public int    MonsterAttack  { get; set; }
+    public int    MonsterDefense { get; set; }
+    public int    MonsterGold    { get; set; }
+}
---
+++ Drag_Static_Site/Models/CombatPlayer.cs
@@ -0,0 +1,9 @@
+﻿namespace Drag_Static_Site.Models;
+
+public class CombatPlayer
+{
+    public string Name    { get; set; }
+    public int    Hp      { get; set; }
+    public int    Attack  { get; set; }
+    public int    Defense { get; set; }
+}
